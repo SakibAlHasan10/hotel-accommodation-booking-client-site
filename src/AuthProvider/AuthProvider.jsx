b2f1af627729fff2ee321ab7284bca4554461d85
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-11-05 10:09:27 +0600

    added use auth

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,12 +1,19 @@
 import { createContext, useState } from "react";
 import PropTypes from "prop-types"
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../configs/firebase/firebase.config";
 export const AuthContext= createContext()
 const AuthProvider = ({children}) => {
     const [theme, setTheme]= useState(true)
     const [isLoading, setIsLoading]=useState(true)
     const [user, setUser]= useState(null)
-
-
+    
+// sign in with google
+const googleProvider = new GoogleAuthProvider()
+const signInWithGoogle =()=>{
+    setIsLoading(true)
+    return signInWithPopup(auth, googleProvider)
+}
 
 
 
@@ -19,6 +26,7 @@ const AuthProvider = ({children}) => {
         isLoading,
         user,
         setTheme,
+        signInWithGoogle,
     }
     return (
         <AuthContext.Provider value={authInfo}>
