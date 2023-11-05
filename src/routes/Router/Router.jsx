1a4e6fe0899850e16be6deb38ae4a5e414efae42
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-11-05 11:53:15 +0600

    added signup page

--- src/routes/Router/Router.jsx
+++ src/routes/Router/Router.jsx
@@ -2,22 +2,27 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../../pages/Root/Root";
 import Home from "../../pages/Home/Home";
 import Login from "../../pages/Login/Login";
+import Signup from "../../pages/Signup/Signup";
 
 const router = createBrowserRouter([
-    {
-        path:'/',
-        element: <Root></Root>,
-        // errorElement:
-        children:[
-            {
-                path:'/',
-                element:<Home></Home>
-            },
-            {
-                path:"login",
-                element:<Login></Login>
-            }
-        ]
-    },
-])
+  {
+    path: "/",
+    element: <Root></Root>,
+    // errorElement:
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+  {
+    path: "login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/signup",
+    element: <Signup></Signup>,
+  },
+]);
 export default router;
