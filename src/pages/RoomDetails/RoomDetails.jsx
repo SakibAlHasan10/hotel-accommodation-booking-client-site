dbbdd1918913f7353e189b43276505d8aed0c804
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-11-06 10:51:23 +0600

    added room images in details pages

--- src/pages/RoomDetails/RoomDetails.jsx
+++ src/pages/RoomDetails/RoomDetails.jsx
@@ -1,17 +1,49 @@
 import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const RoomDetails = () => {
-    const {id} = useParams()
-    axios.get(`http://localhost:5173/details/${id}`)
-    .then(res=>{
-        console.log(res.data)
-    })
-    return (
-        <div>
-            room
+  const [room, setRoom] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    axios.get(`http://localhost:5000/rooms/${id}`).then((res) => {
+      setRoom(res.data);
+    });
+  }, [id]);
+  const {
+    _id,
+    RoomDescription,
+    PricePerNight,
+    RoomSize,
+    Availability,
+    RoomImages,
+    roomImages,
+    SpecialOffers,
+    Rating,
+    Confirm,
+    Title,
+    Location,
+    Reviews,
+  } = room;
+  console.log(RoomImages)
+  return (
+    <div className="mt-16 py-10 bg-base-300 ">
+      <div className=" flex max-w-6xl mx-auto p-5 bg-base-100 rounded-xl">
+        <div className="w-3/5 ">
+          <img src={RoomImages} alt="" className="w-full h-full rounded-lg" />
         </div>
-    );
+        <div className="w-2/5 gap-3 flex flex-col">
+            {
+                RoomImages?.map(room=><div key={room} className="">
+
+                    <img src={room} alt="" className="w-36 h-36 mx-4 rounded-lg" />
+                </div>)
+            }
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RoomDetails;
