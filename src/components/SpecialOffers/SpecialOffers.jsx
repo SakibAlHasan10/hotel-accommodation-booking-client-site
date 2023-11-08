2c55e9febdf33536cceff58333cdca06a9f58690
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-11-08 23:59:24 +0600

    added sum text

--- src/components/SpecialOffers/SpecialOffers.jsx
+++ src/components/SpecialOffers/SpecialOffers.jsx
@@ -5,7 +5,7 @@ const SpecialOffers = () => {
     const { loadRoom } = useAuth();
     return (
         <div className="max-w-screen-lg mx-auto px-6  mt-6 bg-base-100 py-6 rounded-2xl">
-            SpecialOffers
+            <p className="my-5 text-2xl font-semibold">Special Offers</p>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
             {loadRoom?.slice(3,6).map((room) => (
             <SingleRoom key={room._id} room={room}></SingleRoom>
